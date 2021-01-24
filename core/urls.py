99cd72f83e280973093de95712613eb80445719d
Fábio Vitor <fabvitor2010@gmail.com>
2021-01-24 20:29:14 -0300

    Correção basename

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from .views import home_api, LinkViewSet
 from rest_framework import routers
 
 router = routers.SimpleRouter(trailing_slash='optional')
-router.register(r'links', LinkViewSet, base_name='links')
+router.register(r'links', LinkViewSet, basename='links')
 
 urlpatterns = [
     re_path(r'^$', home_api),
